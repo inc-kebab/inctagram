8b038ab41c93f2618b18799dbcba69ecda3ddc45
tazalov <tazalov9@gmail.com>
2024-02-10 17:05:36 +0300

    fix: add svgr & alias supports in storybook
    Add Inter font

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,33 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react'
+
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@/app/styles/index.scss'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: 'Dark',
+      values: [
+        {
+          name: 'Dark',
+          value: '#000',
+        },
+        {
+          name: 'Light',
+          value: '#f4f4f4',
+        },
+      ],
+    },
   },
-};
+}
 
-export default preview;
+export default preview
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import type { AppProps } from 'next/app'
 
 import { useLoader } from '@/shared/hooks'
 
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 import '@/app/styles/index.scss'
 import '@/app/styles/nprogress.scss'
 
