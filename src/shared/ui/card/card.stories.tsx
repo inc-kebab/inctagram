e3820b1ff86a6cfb28b1db29b1ab884ca45909d1
Artur Khareksian <r2mail1122@gmail.com>
2024-02-11 16:32:25 +0300

    fix: add card

--- src/shared/ui/card/card.stories.tsx
+++ src/shared/ui/card/card.stories.tsx
@@ -12,5 +12,7 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    children: <div>Card</div>,
+  },
 }
--- src/shared/ui/card/card.tsx
+++ src/shared/ui/card/card.tsx
@@ -5,12 +5,6 @@ import clsx from 'clsx'
 import s from './card.module.scss'
 
 type CardProps = ComponentPropsWithoutRef<'div'>
-export const Card = ({ children, className, ...rest }: CardProps) => {
-  const cardCN = clsx(s.card, className)
-
-  return (
-    <div className={cardCN} {...rest}>
-      {children}
-    </div>
-  )
-}
+export const Card = ({ className, ...rest }: CardProps) => (
+  <div className={clsx(s.card, className)} {...rest} />
+)
