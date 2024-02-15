09db441ef022a9a2b542dae3fdb54a2ff14c7d07
Artur Khareksian <r2mail1122@gmail.com>
2024-02-15 18:40:07 +0300

    init: email verification

--- src/pages/email-verification/index.tsx
+++ src/pages/email-verification/index.tsx
@@ -1,36 +1,13 @@
-'use client'
-import { useEffect, useState } from 'react'
-
 import { Button } from '@/shared/ui/Button'
 import { Typography } from '@/shared/ui/Typography'
 import Image from 'next/image'
 
 import s from './EmailVerification.module.scss'
 
-function useWindowDimensions() {
-  const [currentWidth, setCurrentWidth] = useState(
-    typeof window !== 'undefined' ? window.innerWidth : 0
-  )
-
-  useEffect(() => {
-    function handleResize() {
-      setCurrentWidth(window.innerWidth)
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
-  return currentWidth
-}
-
 const EmailVerification = () => {
-  const width = useWindowDimensions()
-
   return (
     <section className={s.wrapper}>
-      <Typography className={s.title} textAlign="center" variant={width > 700 ? 'h1' : 'regular16'}>
+      <Typography as="h1" className={s.title} textAlign="center" variant="h1">
         Email verification link expired
       </Typography>
       <Typography className={s.text} textAlign="center" variant="regular16">
