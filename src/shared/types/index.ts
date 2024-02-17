c43d9daf4c89cb9596b7fb793f6ed43df49cd9e9
tazalov <tazalov9@gmail.com>
2024-02-17 09:11:19 +0300

    hotfix: types for polymorph components (Button & Typography)
    Refactor stories

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,5 +1,26 @@
-import { ElementRef, ElementType, Ref } from 'react'
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  ElementType,
+  ForwardedRef,
+  PropsWithChildren,
+} from 'react'
 
-export interface PolymorphRef<T extends ElementType> {
-  ref?: Ref<ElementRef<T>>
+interface ForwardedRefProp<T extends ElementType> {
+  ref?: ForwardedRef<ElementRef<T>>
 }
+
+interface AsComponentProp<T extends ElementType> {
+  asComponent?: T
+}
+
+export type PolymorphComponentProps<T extends ElementType, P = {}> = PropsWithChildren<
+  P & AsComponentProp<T>
+> &
+  Omit<ComponentPropsWithoutRef<T>, keyof (AsComponentProp<T> & P)>
+
+export type PolymorphComponentPropsWithRef<T extends ElementType, P = {}> = PolymorphComponentProps<
+  T,
+  P
+> &
+  ForwardedRefProp<T>
