9350853b465384c14bddb4c8d7249df7e78da51f
tazalov <tazalov9@gmail.com>
2024-02-19 19:39:42 +0300

    IN-42 feat: add base-api, reauth flow (mutex), config store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,25 @@
 import type { AppProps } from 'next/app'
 
+import { Provider } from 'react-redux'
+
+import { store } from '@/app'
 import { useLoader } from '@/shared/hooks'
 import { ToastProvider } from '@/widgets/toast'
 
 import 'react-toastify/dist/ReactToastify.css'
+import '@/app/styles/index.scss'
+import '@/app/styles/nprogress.scss'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
-import '@/app/styles/nprogress.scss'
-import '@/app/styles/index.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   useLoader()
 
   return (
-    <>
+    <Provider store={store}>
       <Component {...pageProps} />
       <ToastProvider />
-    </>
+    </Provider>
   )
 }
