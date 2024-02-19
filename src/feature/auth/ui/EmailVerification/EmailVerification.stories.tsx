71eea9d88e2bcd16dbc2b999a7d384c5909245fe
Artur Khareksian <r2mail1122@gmail.com>
2024-02-19 17:36:18 +0300

    IN-37 refactor: email verification

--- src/feature/auth/ui/EmailVerification/EmailVerification.stories.tsx
+++ src/feature/auth/ui/EmailVerification/EmailVerification.stories.tsx
@@ -3,8 +3,19 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { EmailVerification } from './EmailVerification'
 
 const meta = {
+  argTypes: {
+    onResendLink: {
+      action: 'resend verification link',
+      description: 'Callback for resend verification link',
+    },
+  },
   component: EmailVerification,
   parameters: {
+    docs: {
+      description: {
+        component: 'The component allows you to do a resubmission of the verification link',
+      },
+    },
     layout: 'centered',
   },
   tags: ['autodocs'],
@@ -15,5 +26,7 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    onResendLink: () => {},
+  },
 }
--- src/feature/auth/ui/EmailVerification/EmailVerification.tsx
+++ src/feature/auth/ui/EmailVerification/EmailVerification.tsx
@@ -5,23 +5,27 @@ import Image from 'next/image'
 import s from './EmailVerification.module.scss'
 
 interface Props {
-  something?: any
+  onResendLink?: () => void
 }
-export const EmailVerification = ({ something }: Props) => {
+export const EmailVerification = ({ onResendLink }: Props) => {
   return (
     <section className={s.wrapper}>
-      <Typography as="h1" className={s.title} textAlign="center" variant="h1">
+      <Typography asComponent="h1" className={s.title} textAlign="center" variant="h1">
         Email verification link expired
       </Typography>
       <Typography className={s.text} textAlign="center" variant="regular16">
         Looks like the verification link has expired. Not to worry, we can send the link again
       </Typography>
-      <Button className={s.button} onClick={something}>
+      <Button className={s.button} onClick={onResendLink}>
         Resend verification link
       </Button>
-      <div className={s.imageWrapper}>
-        <Image alt="Email verification" fill objectFit="cover" src="/email-verification.svg" />
-      </div>
+      <Image
+        alt="Email verification"
+        className={s.image}
+        height={0}
+        src="/email-verification.svg"
+        width={0}
+      />
     </section>
   )
 }
