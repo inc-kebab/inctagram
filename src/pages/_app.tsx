58f9c3d2e4fbce47a5690d2c940524582517dcb3
tazalov <tazalov9@gmail.com>
2024-02-19 19:11:08 +0300

    IN-42 feat: add toast widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,22 @@
 import type { AppProps } from 'next/app'
 
 import { useLoader } from '@/shared/hooks'
+import { ToastProvider } from '@/widgets/toast'
 
+import 'react-toastify/dist/ReactToastify.css'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
-import '@/app/styles/index.scss'
 import '@/app/styles/nprogress.scss'
+import '@/app/styles/index.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   useLoader()
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastProvider />
+    </>
+  )
 }
