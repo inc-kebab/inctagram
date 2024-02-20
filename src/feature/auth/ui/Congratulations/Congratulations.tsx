750769318b7341372859e58399afe2b85b55f219
Archi999999 <archi999999@gmail.com>
2024-02-20 21:08:23 +0300

    IN-38: feat: mail verification success (Congratulation.tsx)

---
+++ src/feature/auth/ui/Congratulations/Congratulations.tsx
@@ -0,0 +1,24 @@
+import img from '@/../public/congratulations.png'
+import { Button } from '@/shared/ui/Button'
+import { Typography } from '@/shared/ui/Typography'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import s from './Congratulations.module.scss'
+
+export const Congratulations = () => {
+  return (
+    <section className={s.wrapper}>
+      <Typography asComponent="h1" className={s.title} variant="h1">
+        Congratulations!
+      </Typography>
+      <Typography asComponent="p" className={s.text} textAlign="center" variant="regular16">
+        Your email has been confirmed
+      </Typography>
+      <Button asComponent={Link} className={s.button} href="auth/sign-in">
+        Sign in
+      </Button>
+      <Image alt="Congratulation image" className={s.image} height={0} src={img} width={0} />
+    </section>
+  )
+}
---
+++ src/feature/auth/ui/Congratulations/Congrlatulations.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Congratulations } from '../index'
+
+const meta: Meta<typeof Congratulations> = {
+  component: Congratulations,
+  parameters: {
+    description: {
+      component: "This page confirms that the user's email has been successfully confirmed.",
+    },
+  },
+  tags: ['autodocs'],
+  title: 'auth/Congratulations',
+}
+
+export default meta
+
+type Story = StoryObj<typeof Congratulations>
+
+export const Default: Story = {
+  args: {},
+}
