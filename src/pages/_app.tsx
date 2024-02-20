cb0c8d8e1eef34b03e464f940bd23a91e3ecfb9a
tazalov <tazalov9@gmail.com>
2024-02-20 19:37:27 +0300

    IN-43 feat: add layout logic in _app with types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,33 @@
 import type { AppProps } from 'next/app'
 
+import { Fragment } from 'react'
 import { Provider } from 'react-redux'
 
 import { store } from '@/app'
 import { useLoader } from '@/shared/hooks'
+import { Page } from '@/shared/types/layout'
 import { ToastProvider } from '@/widgets/toast'
 
 import 'react-toastify/dist/ReactToastify.css'
-import '@/app/styles/index.scss'
-import '@/app/styles/nprogress.scss'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
+import '@/app/styles/nprogress.scss'
+import '@/app/styles/index.scss'
 
-export default function App({ Component, pageProps }: AppProps) {
+type Props = AppProps & {
+  Component: Page
+}
+
+export default function App({ Component, pageProps }: Props) {
   useLoader()
+
+  const getLayout = Component.getLayout ?? (page => page)
+  const Layout = Component.layout ?? Fragment
 
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
       <ToastProvider />
     </Provider>
   )
