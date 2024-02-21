cb70b2e6c2d14f5d670f731c52793a59c6195e5f
tazalov <tazalov9@gmail.com>
2024-02-21 09:02:08 +0300

    IN-43 feat: add layouts for pages, add public api for layout widget

--- src/pages/messenger/index.tsx
+++ src/pages/messenger/index.tsx
@@ -1,5 +1,14 @@
-const Messenger = () => {
+import { ReactElement } from 'react'
+
+import { Page } from '@/shared/types/layout'
+import { SidebarLayout } from '@/widgets/layout'
+
+const Messenger: Page = () => {
   return <div>Messenger page</div>
+}
+
+Messenger.getLayout = (page: ReactElement) => {
+  return <SidebarLayout>{page}</SidebarLayout>
 }
 
 export default Messenger
