4c15b62966abaf8834e3ad0efa4ae9a8b4127280
tazalov <tazalov9@gmail.com>
2024-03-03 12:51:15 +0300

    hotfix: stories args for SignIn & SignUp

--- src/feature/auth/ui/SignUpForm/SignUpForm.stories.tsx
+++ src/feature/auth/ui/SignUpForm/SignUpForm.stories.tsx
@@ -28,4 +28,9 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    hrefGithub: '#',
+    hrefGoogle: '#',
+  },
+}
