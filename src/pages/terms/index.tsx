d3755b80bcfa063af602a97026b1c2cf19eb6ecd
tazalov <tazalov9@gmail.com>
2024-03-11 20:18:43 +0300

    IN-124 refactor: terms & privacy hide link flow

--- src/pages/terms/index.tsx
+++ src/pages/terms/index.tsx
@@ -18,11 +18,13 @@ const Terms: Page = memo(() => {
 
   return (
     <div className={s.wrapper}>
-      <BackToPage
-        className={s.link}
-        onNavigate={handleNavigateToPrevPage}
-        title={router.query.sender ? t.button.back : t.button.backToSignUp}
-      />
+      {!router.query.sender && (
+        <BackToPage
+          className={s.link}
+          onNavigate={handleNavigateToPrevPage}
+          title={t.button.backToSignUp}
+        />
+      )}
       <Typography asComponent="h1" className={s.title} textAlign="center" variant="h1">
         {t.pages.terms.title}
       </Typography>
