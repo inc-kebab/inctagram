b1cb182a66f7a6a4f81b099d4a4aeb183e401792
tazalov <tazalov9@gmail.com>
2024-03-12 18:33:02 +0300

    IN-124 refactor: types, regex, styles

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -12,7 +12,7 @@ const Profile: Page = () => {
   const { data, isLoading } = useGetMyProfileQuery()
 
   if (isLoading) {
-    return <Loader />
+    return <Loader fullHeight />
   }
 
   return (
