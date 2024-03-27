5fab6ca447b0e1a3472de69a37c349df2f1cf9ec
tazalov <tazalov9@gmail.com>
2024-03-27 20:26:21 +0300

    tests with change locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,16 +7,14 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get('accessToken')?.value
   const nextUrlPath = req.nextUrl.pathname
 
-  const isLocaleDifferent = !!localeCookie && req.nextUrl.locale !== localeCookie
+  const isLocaleDiffer = !!localeCookie && req.nextUrl.locale !== localeCookie
 
   const isGeneral = nextUrlPath.startsWith('/general/')
 
   const isPublic = nextUrlPath.startsWith('/auth/') || nextUrlPath === '/'
 
   if (isGeneral) {
-    if (isLocaleDifferent) {
-      req.cookies.set('NEXT_LOCALE', localeCookie)
-
+    if (isLocaleDiffer) {
       return NextResponse.redirect(new URL(`/${localeCookie}${nextUrlPath}?${params}`, req.url))
     } else {
       return NextResponse.next()
@@ -24,9 +22,7 @@ export function middleware(req: NextRequest) {
   }
 
   if (token && isPublic) {
-    if (isLocaleDifferent) {
-      req.cookies.set('NEXT_LOCALE', localeCookie)
-
+    if (isLocaleDiffer) {
       return NextResponse.redirect(new URL(`/${localeCookie}${AppRoutes.HOME}?${params}`, req.url))
     } else {
       return NextResponse.redirect(new URL(`${AppRoutes.HOME}?${params}`, req.url))
@@ -34,18 +30,14 @@ export function middleware(req: NextRequest) {
   }
 
   if (!token && !isPublic) {
-    if (isLocaleDifferent) {
-      req.cookies.set('NEXT_LOCALE', localeCookie)
-
+    if (isLocaleDiffer) {
       return NextResponse.redirect(new URL(`/${localeCookie}${AppRoutes.MAIN}`, req.url))
     } else {
       return NextResponse.redirect(new URL(`${AppRoutes.MAIN}`, req.url))
     }
   }
 
-  if (isLocaleDifferent) {
-    req.cookies.set('NEXT_LOCALE', localeCookie)
-
+  if (isLocaleDiffer) {
     return NextResponse.redirect(new URL(`/${localeCookie}${nextUrlPath}?${params}`, req.url))
   }
 
