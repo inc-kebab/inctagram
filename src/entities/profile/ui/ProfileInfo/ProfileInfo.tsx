072eeba0cc705b1349f4288a7fc913c87fd86900
tazalov <tazalov9@gmail.com>
2024-03-27 13:19:46 +0300

    IN-194 refactor: styles after review

--- src/entities/profile/ui/ProfileInfo/ProfileInfo.tsx
+++ src/entities/profile/ui/ProfileInfo/ProfileInfo.tsx
@@ -3,7 +3,6 @@ import { Avatar } from '@/shared/ui/Avatar'
 import { Button } from '@/shared/ui/Button'
 import { Typography } from '@/shared/ui/Typography'
 import clsx from 'clsx'
-import Image from 'next/image'
 import Link from 'next/link'
 
 import s from './ProfileInfo.module.scss'
@@ -20,31 +19,29 @@ export const ProfileInfo = ({ className, userData }: Props) => {
     <div className={clsx(s.ProfileInfo, className)}>
       <Avatar avatarUrl={userData?.avatar} circle wrapperSize={200} />
       <div className={s.info}>
-        <div className={s.name}>
-          <Typography asComponent="h2" variant="h1">
-            {userData?.username}
-          </Typography>
-          <Button
-            asComponent={Link}
-            href={{ pathname: AppRoutes.PROFILE_SETTINGS, query: { tab: 'general' } }}
-            variant="secondary"
-          >
-            Profile settings
-          </Button>
-        </div>
+        <Typography asComponent="h2" className={s.name} variant="h1">
+          {userData?.username}
+        </Typography>
         <ul className={s.subscriber}>
-          <li>
-            <span>2 218</span> Following
+          <li className={s.item}>
+            <span className={s.count}>2 218</span> Following
           </li>
-          <li>
-            <span>2 358</span> Followers
+          <li className={s.item}>
+            <span className={s.count}>2 358</span> Followers
           </li>
-          <li>
-            <span>2 764</span> Publications
+          <li className={s.item}>
+            <span className={s.count}>2 764</span> Publications
           </li>
         </ul>
-        <Typography variant="regular16">{userData?.aboutMe || 'about me'}</Typography>
+        {userData?.aboutMe && <Typography variant="regular16">{userData.aboutMe}</Typography>}
       </div>
+      <Button
+        asComponent={Link}
+        href={{ pathname: AppRoutes.PROFILE_SETTINGS, query: { tab: 'general' } }}
+        variant="secondary"
+      >
+        Profile settings
+      </Button>
     </div>
   )
 }
