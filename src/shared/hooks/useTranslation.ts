c9a84976a8bd51fdb6b9e9fa50f85f30dc89988e
tazalov <tazalov9@gmail.com>
2024-03-28 14:13:02 +0300

    fix: locale redirects
    Add protect for providers route

--- src/shared/hooks/useTranslation.ts
+++ src/shared/hooks/useTranslation.ts
@@ -1,19 +1,8 @@
-import { useEffect } from 'react'
-
 import { en, ru } from '@/../locales'
-import { getCookie, setCookie } from 'cookies-next'
 import { useRouter } from 'next/router'
 
 export const useTranslation = () => {
   const router = useRouter()
-
-  useEffect(() => {
-    const localeCookie = getCookie('NEXT_LOCALE')
-
-    if (!localeCookie) {
-      setCookie('NEXT_LOCALE', router.locale, { maxAge: 100 * 365 * 24 * 60 * 60 }) // 1year
-    }
-  }, [])
 
   return { locale: router.locale, t: router.locale === 'ru' ? ru : en }
 }
--- src/widgets/lang-switcher/model/hooks/useLangSwitcher.ts
+++ src/widgets/lang-switcher/model/hooks/useLangSwitcher.ts
@@ -1,14 +1,10 @@
-import { setCookie } from 'cookies-next'
 import { useRouter } from 'next/router'
 
 export const useLangSwitcher = () => {
   const { asPath, defaultLocale, locale, pathname, query, replace } = useRouter()
 
   const changeLocale = (locale: string) => {
-    setCookie('NEXT_LOCALE', locale, { maxAge: 100 * 365 * 24 * 60 * 60 })
-    void replace({ pathname, query }, asPath, {
-      locale,
-    })
+    void replace({ pathname, query }, asPath, { locale })
   }
 
   return { changeLocale, defaultLocale, locale }
