dc1aef9d89abea6cee2cb4952e60a8fd1e541ad9
Artur Khareksian <r2mail1122@gmail.com>
2024-04-02 19:45:55 +0300

    hotfix: ZoomIn

--- src/entities/post/ui/ZoomIn/ZoomIn.tsx
+++ src/entities/post/ui/ZoomIn/ZoomIn.tsx
@@ -1,7 +1,10 @@
-import { useDispatch } from 'react-redux'
+import { useState } from 'react'
 
+import { useAppDispatch } from '@/app'
 import { Maximize } from '@/shared/assets/icons/outline'
 import { Button } from '@/shared/ui/Button'
+import { Slider } from '@/shared/ui/Slider'
+import clsx from 'clsx'
 
 import s from './ZoomIn.module.scss'
 
@@ -9,28 +12,56 @@ import { postsActions } from '../../model/slice/post-slice'
 
 type Props = {
   aspect: number
+  className?: string
   imageURL: string
   zoom: number
 }
 
-export const ZoomIn = ({ aspect, imageURL, zoom }: Props) => {
-  const dispatch = useDispatch()
+export const ZoomIn = (props: Props) => {
+  const { aspect, className, imageURL, zoom } = props
+  const [isOpenZoom, setIsOpenZoom] = useState(false)
+  const dispatch = useAppDispatch()
 
   const handleSetAspect = () => {
     if (aspect === 0) {
       dispatch(postsActions.updateImage({ aspect: 1, imageURL }))
+    } else if (aspect > 0) {
+      dispatch(postsActions.updateImage({ aspect, imageURL }))
     }
+
+    if (isOpenZoom) {
+      dispatch(postsActions.updateImage({ aspect: 0, imageURL }))
+    }
+
+    setIsOpenZoom(!isOpenZoom)
+  }
+
+  const handleZoom = (value: number) => {
+    dispatch(postsActions.updateImage({ imageURL, zoom: value }))
   }
 
   return (
-    <Button
-      className={s.expandBtn}
-      disabled={aspect !== 0}
-      onClick={handleSetAspect}
-      startIcon={<Maximize height={24} width={24} />}
-      variant="text"
-    >
-      x{zoom}
-    </Button>
+    <div className={className}>
+      <Button
+        className={s.zoomInBtn}
+        onClick={handleSetAspect}
+        startIcon={<Maximize className={clsx(isOpenZoom && s.isOpenIcon)} height={24} width={24} />}
+      />
+
+      {isOpenZoom && (
+        <div className={s.zoomInContainer}>
+          <Slider
+            className={s.slider}
+            max={2.5}
+            min={1}
+            onValueChange={value => handleZoom(value[0])}
+            sliderThumbClass={s.sliderThumbClass}
+            sliderTrackClass={s.sliderTrackClass}
+            step={0.02}
+            value={[zoom]}
+          />
+        </div>
+      )}
+    </div>
   )
 }
