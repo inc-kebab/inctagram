7f181babe101096d4ee6a25f19dccd1a792baa88
Archi999999 <archi999999@gmail.com>
2024-04-08 17:41:23 +0300

    IN-205 feat: add redirect notFound

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export async function revalidate(req: NextApiRequest, res: NextApiResponse) {
+export default async function revalidate(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.secret !== process.env.NEXT_PUBLIC_TOKEN_FOR_SSG) {
     return res.status(401).json({ message: 'Invalid token' })
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,48 +7,55 @@ import {
 } from '@/feature/public-posts'
 import { getTotalProfileCount, useGetTotalProfileCountQuery } from '@/feature/public-profile'
 import { publicProfileApi } from '@/feature/public-profile/api/public-profile-api'
-import { revalidate } from '@/pages/api/revalidate'
-import { useTranslation } from '@/shared/hooks/useTranslation'
+import { DefenderAuthRoute } from '@/shared/helpers/hoc'
+import { Page } from '@/shared/types/layout'
 import { PublicLayout } from '@/widgets/layout'
 
 import s from './mainPublic.module.scss'
 
 export const getStaticProps = wrapper.getStaticProps(store => async () => {
-  store.dispatch(getTotalProfileCount.initiate(undefined, { forceRefetch: 5 }))
-  // store.dispatch(getTotalProfileCount.initiate(undefined, { forceRefetch: true }))
   store.dispatch(
-    getAllPublicPosts.initiate({ pageSize: 4, sortDirection: 'desc' }, { forceRefetch: 5 })
+    getTotalProfileCount.initiate(undefined, {
+      forceRefetch: true,
+    })
   )
-  // store.dispatch(
-  //   getAllPublicPosts.initiate({ pageSize: 4, sortDirection: 'desc' }, { forceRefetch: true })
-  // )
+  store.dispatch(
+    getAllPublicPosts.initiate(
+      { pageSize: 4, sortDirection: 'desc' },
+      {
+        forceRefetch: true,
+      }
+    )
+  )
 
-  await Promise.all(store.dispatch(publicProfileApi.util.getRunningQueriesThunk()))
+  const allRes = await Promise.all(store.dispatch(publicProfileApi.util.getRunningQueriesThunk()))
+
+  //? don't checked work
+  if (!allRes) {
+    return {
+      notFound: true,
+    }
+  }
 
   return {
     props: {},
-    // revalidate: 5,
+    revalidate: 60,
   }
 })
 
-const Public = () => {
+const Public: Page = () => {
   const { data } = useGetTotalProfileCountQuery()
-  const posts = useGetAllPublicPostsQuery({ pageSize: 4, sortDirection: 'desc' }).data?.items
-  const { t } = useTranslation()
-
-  const handler = () => {
-    fetch('/api/revalidate?secret=eKYdp4DByptcg8wbxJKsHrz4IT2ozqVM', { method: 'GET' })
-  }
+  const { data: dataPosts } = useGetAllPublicPostsQuery({ pageSize: 4, sortDirection: 'desc' })
 
   return (
-    <PublicLayout title={t.pages.main.metaTitle}>
-      <div className={s.container}>
-        <CounterRegisteredUsers count={data?.totalUsersCount} />
-        <PublicPostsList posts={posts} />
-      </div>
-      <button onClick={handler}>reval</button>
-    </PublicLayout>
+    <div className={s.container}>
+      <CounterRegisteredUsers count={data?.totalUsersCount} />
+      <PublicPostsList posts={dataPosts?.items} />
+    </div>
   )
 }
 
-export default Public
+Public.getLayout = (page, t) => {
+  return <PublicLayout title={t.pages.main.metaTitle}>{page}</PublicLayout>
+}
+export default DefenderAuthRoute(Public)
