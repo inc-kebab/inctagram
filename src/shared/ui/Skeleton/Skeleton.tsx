a4eed2ced6841ed9171dff6d766a8f5af2a46f58
tazalov <tazalov9@gmail.com>
2024-04-17 11:09:39 +0300

    hotfix: skeleton props

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -1,11 +1,22 @@
+import { CSSProperties } from 'react'
+
 import clsx from 'clsx'
 
 import s from './Skeleton.module.scss'
 
 interface Props {
+  border?: string
   className?: string
+  height?: number | string
+  width?: number | string
 }
 
-export const Skeleton = ({ className }: Props) => {
-  return <div className={clsx(s.Skeleton, className)}></div>
+export const Skeleton = ({ border, className, height, width }: Props) => {
+  const styles: CSSProperties = {
+    borderRadius: border,
+    height,
+    width,
+  }
+
+  return <div className={clsx(s.Skeleton, className)} style={styles}></div>
 }
