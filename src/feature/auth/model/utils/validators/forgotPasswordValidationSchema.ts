754b68be9f5453c73d1cc78961d95043715469de
tazalov <tazalov9@gmail.com>
2024-05-13 18:03:09 +0300

    hotfix: recaptcha flow

--- src/feature/auth/model/utils/validators/forgotPasswordValidationSchema.ts
+++ src/feature/auth/model/utils/validators/forgotPasswordValidationSchema.ts
@@ -8,12 +8,18 @@ export const forgotPasswordSchema = (t: LocaleType) =>
       .trim()
       .min(1, t.validation.required)
       .email({ message: t.validation.emailVerification }),
-    recaptcha: z
-      .string()
-      .nullable()
-      .refine(value => value, {
+    recaptcha: z.string().refine(
+      value => {
+        if (value === null) {
+          return true
+        }
+
+        return true
+      },
+      {
         message: t.validation.required,
-      }),
+      }
+    ),
   })
 
 export type ForgotPasswordFormValues = z.infer<ReturnType<typeof forgotPasswordSchema>>
