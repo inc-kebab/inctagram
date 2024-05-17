9f608801ead5d58a61bfd2299ec4291bde431ed4
tazalov <tazalov9@gmail.com>
2024-05-17 14:28:39 +0300

    IN-286 fix: change z-index for confirm dialog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,13 +12,13 @@ import { setCookie } from 'cookies-next'
 import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router'
 
-import '@/app/styles/index.scss'
-import '@/app/styles/nprogress.scss'
-import '@/shared/ui/Carousel/Carousel.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import '@/shared/ui/Carousel/Carousel.scss'
+import '@/app/styles/nprogress.scss'
+import '@/app/styles/index.scss'
 
 type Props = AppProps & {
   Component: Page
